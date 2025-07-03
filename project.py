2e8c2ff2c03383367198558054ce8fa8ba38d842
joseph miguel <josephchimaobi099@gmail.com>
2025-07-03 03:42:06 +0100

    just created pdf pages and added footers

--- project.py
+++ project.py
@@ -1,11 +1,24 @@
 from fpdf import FPDF
-
+import pandas as pd
 pdf = FPDF(orientation='portrait',unit='mm',format='A4')
-pdf.add_page()
-
-pdf.set_font(family='Times', size=12)
-pdf.cell(w=0, h=10, ln=1, border=1, txt='You making progress bro')
-pdf.set_font(family='Times',size=10)
-pdf.cell(w=0, h=5, ln=1, border=1,txt='Just keep working hard')
+pdf.set_auto_page_break(False)
+df = pd.read_csv('topicss.csv')
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times',style='B', size=12)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=10, ln=1, border=0, txt=row['Topic'])
+    pdf.line(10,22,200,22)
+    #set the footer
+    pdf.ln(265)
+    pdf.set_font(family='Times',style='B',size=4)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10,txt=row['Topic'],align='R')
+    for x in range(row['Pages']-1):
+        pdf.add_page()
+        #set the footer
+        pdf.ln(275)
+        pdf.set_font(family='Times',style='B',size=4)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0,h=10,txt=row['Topic'],align='R')
 pdf.output('result.pdf')
-print(type(pdf))
